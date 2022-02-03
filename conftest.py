574072041483e0593c4f6531f1f2eecda210ede9
Vladimir_Gal'binskiy <gal-vova@ya.ru>
2022-02-03 15:58:32 +0300

    grid add

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 
 
+
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
@@ -14,7 +15,15 @@ def browser(request):
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        browser = webdriver.Remote(
+            command_executor="http://192.168.0.229:4444/wd/hub",
+            desired_capabilities={
+                "browserName": "chrome",
+                "browserVersion": "latest",
+                "video": "True",
+                "platform": "WIN10",
+                "platformName": "windows",
+            })
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         browser = webdriver.Firefox()
